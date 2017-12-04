a8a999baa89048df5892a370e0c99047eb3494ea
mfrisby <mfrisby@student.42.fr>
2017-12-04 12:18:25 +0100

    working email

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php 
     $DB_USER = "camagru";
     $DB_PASSWORD = "root";
-    $DB_DSN = "mysql:host=localhost;dbname=camagru";
-    $DB_DS = "mysql:host=localhost";
+    $DB_DSN = "mysql:host=localhost;dbname=camagru;charset=utf8";
+    $DB_DS = "mysql:host=localhost;charset=utf8";
 ?>
--- email.php
+++ email.php
@@ -2,12 +2,12 @@
     session_start();
     function sendVerifMail($email, $token, $userId) {
         try {
-            $headers  = 'MIME-Version: 1.0' . "\r\n";
-            $headers .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
-            $headers .= 'From: <mfrisby@student.42.fr>' . "\r\n";
-
-            $subject =  'Camagru - Confirmation de votre compte';
+            $subject = "Camagru";
             $msg = "Afin de valider votre compte, merci de cliquer sur ce lien:\n\nhttp//localhost/camagru/confirm.php?id=$userId&token=$token";
+            $headers = "From: mowpy42@gmailcom" . "\r\n" .
+            "Reply-To: mowpy42@gmailcom" . "\r\n" .
+            "X-Mailer: PHP/" . phpversion();
+           
             mail($email, $subject, $msg, $headers);
         }
         catch (PDOException $e) {
