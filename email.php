6c73ce9f26145c9a342b35cc62f15febad533869
mfrisby <mfrisby@student.42.fr>
2017-12-05 17:21:28 +0100

    UPD url email

--- email.php
+++ email.php
@@ -3,7 +3,7 @@
     function sendVerifMail($email, $token, $userId) {
         try {
             $subject = "Camagru";
-            $msg = "Afin de valider votre compte, merci de cliquer sur ce lien:\n\nhttp//localhost/camagru/confirm.php?id=$userId&token=$token";
+            $msg = "Afin de valider votre compte, merci de cliquer sur ce lien:\n\nlocalhost/camagru/confirm.php?id=$userId&token=$token";
             $headers = "From: mowpy42@gmailcom" . "\r\n" .
             "Reply-To: mowpy42@gmailcom" . "\r\n" .
             "X-Mailer: PHP/" . phpversion();
