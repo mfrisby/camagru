4ad3ebcaca10ef35cecd282e419accdfe95b84ce
mfrisby <mfrisby@student.42.fr>
2017-12-14 19:20:47 +0100

    ADD msg upload

--- index.php
+++ index.php
@@ -11,21 +11,18 @@
             echo "<h2 class=\"title\">Login error, wrong password or username.</h2>";
             echo $home;
         }
-        else if (isset($_GET['fileupload'])) {
-            echo "<div class=\"level\">";
-            echo "<h2 class=\"title\">UPLOAD.</h2>";
-            echo "</div>";
-            echo $home;
-        }
-        else if (isset($_GET['noupload'])) {
-            echo "<h2 class=\"title\">NO UPLOAD.</h2>";
-            echo $home;
-        }
         else if (isset($_GET['msglogverified'])) {
             echo "<h2 class=\"title\">I send you a link by email.</br>Please validate your account.</h2>";
             echo $home;
         }
         else if (isset($_SESSION['signup_success']))  {
+            if (isset($_GET['fileupload'])) {
+                $s = alert("Upload succeed", "is-success");
+                echo $s;
+            }
+            if (isset($_GET['noupload'])) {
+                echo (alert("Upload failed", "is-danger"));
+            }
             include("parts/picture.php");
         }
         else {
@@ -34,6 +31,10 @@
                 echo "<h2 class=\"title\">An email has been sent, please confirm your account and login.</h2>";
                 echo $home;
             }
+        }
+        function alert($string, $type) {
+            $s = "<section class=\"hero ".$type."\"><div class=\"hero-body container\">".$string."</div></section>";
+            return $s;
         }
     ?>
 <?php include("parts/footer.html"); ?>
