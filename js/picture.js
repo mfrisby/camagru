9b0ed6292acd8d5abb8a3f67275fddc141ca005c
mfrisby <mfrisby@student.42.fr>
2018-01-01 18:55:03 +0100

    RMV table + BUG cam & file

--- js/picture.js
+++ js/picture.js
@@ -1,26 +1,29 @@
-(function() {
+(function () {
+	
 	var width = 400;
 	var height = 400;
 
 	var streaming = false;
-	var isplaying = false;
-  
+
 	var video = document.getElementById('video');
+
+	//draw video in canvas with png
 	var videoCanvas = document.getElementById('videoCanvas');
-	var canvas = document.createElement("canvas");
-	var photo = document.getElementById('photo');
-	var context = canvas.getContext('2d');
 	var videoContext = videoCanvas.getContext('2d');
 
-	var img = new Image();
+	//draw final picture in canvas
+	var canvas = document.createElement("canvas");
+	var context = canvas.getContext('2d');
+
 	var path = "";
+	var img = new Image();
+
 	var hat = document.getElementById('hat');
 	var fire = document.getElementById('fire');
 	var beer = document.getElementById('beer');
 	var hatborder = document.getElementById('trHat');
 	var fireborder = document.getElementById('trFire');
 	var beerborder = document.getElementById('trBeer');
-	var tmp = document.getElementById("tmp");
 
 	var startbutton = document.getElementById('startbutton');
 	var sendbutton = document.getElementById('sendbutton');
@@ -33,195 +36,225 @@
 	var pngX = 0;
 	var pngY = 0;
 
-	var formfile = document.getElementById('formFile');
-
 	function startup() {
-  
+
 		navigator.getMedia = (
 			navigator.getUserMedia ||
 			navigator.webkitGetUserMedia ||
 			navigator.mozGetUserMedia ||
 			navigator.msGetUserMedia);
-  
+
 		navigator.getMedia({
-		  video: true,
-		  audio: false
-		},function(stream) {
-				if (navigator.mediaDevices.getUserMedia) {
-					video.mozSrcObject = stream;
-				}
-				else {
-					var vendorURL = window.URL || window.webkitURL;
-					video.src = vendorURL.createObjectURL(stream);
-				}
-				isplaying = true;
-				video.play();
-		},function(err) {
-		  console.log("An error occured! " + err);
-		});
-  
-		video.addEventListener('canplay', function(ev){
-			if (!streaming) {
-				height = video.videoHeight / (video.videoWidth/width);
-				video.setAttribute('width', width);
-				video.setAttribute('height', height);
-				canvas.setAttribute('width', width);
-				canvas.setAttribute('height', height);
-				videoCanvas.setAttribute('width', width);
-				videoCanvas.setAttribute('height', height);
-				draw_video();
-				streaming = true;
+			video: true,
+			audio: false
+		}, function (stream) {
+			if (navigator.mediaDevices.getUserMedia) {
+				video.mozSrcObject = stream;
 			}
-		}, false);
-	
-		function takepicture() {
+			else {
+				var vendorURL = window.URL || window.webkitURL;
+				video.src = vendorURL.createObjectURL(stream);
+			}
+			video.play();
+		}, function (err) {
+			//do stuff
+		});
+	}
+
+	video.addEventListener('canplay', function (ev) {
+		if (!streaming) {
+			height = video.videoHeight / (video.videoWidth / width);
+			video.setAttribute('width', width);
+			video.setAttribute('height', height);
+			canvas.setAttribute('width', width);
+			canvas.setAttribute('height', height);
+			videoCanvas.setAttribute('width', width);
+			videoCanvas.setAttribute('height', height);
+			draw_video();
+			streaming = true;
+		}
+	}, false);
+
+	function takepicture() {
+		if (!streaming) {
+			var data = videoCanvas.toDataURL('image/png');
+			show_picture(data);
+		}
+		else {
 			if (width && height) {
 				canvas.width = width;
 				canvas.height = height;
 				context.drawImage(video, 0, 0, width, height);
 				if (path != "") {
-					img.onload = function() {
+					img.onload = function () {
 						context.drawImage(img, pngX, pngY);
 						var data = canvas.toDataURL('image/png');
-						save_picture(data, path);
-						show_picture(data)
+						send_cam_pic(data, path);
+						show_picture(data);
 					}
 					img.src = path;
 				}
 			}
 		}
-		function show_picture(data) {
-				var tr = document.createElement("tr");
-				tr.innerHTML += "<img src=\"" + data + "\"/>";
-				tmp.appendChild(tr);
+	}
+	function show_picture(data) {
+		var table = document.getElementById("table");
+		table.innerHTML += "<img src=\"" + data + "\"/></br>";
+	}
+
+	startbutton.addEventListener('click', function (ev) {
+		takepicture();
+		ev.preventDefault();
+	}, false);
+
+	sendbutton.addEventListener('click', function (ev) {
+		var pic = new Image();
+		var png = new Image();
+		if (width && height) {
+			canvas.width = width;
+			canvas.height = height;
+
+			videoCanvas.setAttribute('width', width);
+			videoCanvas.setAttribute('height', height);
+
+			var input = document.getElementById("filepng");
+			var fReader = new FileReader();
+			fReader.readAsDataURL(input.files[0]);
+			fReader.onloadend = function (event) {
+				pic.onload = function () {
+					draw_pic(pic);
+				}
+				pic.src = event.target.result;
+			}
 		}
-		function save_picture(dataurl, path) {
-			if (window.XMLHttpRequest) {
-				xmlhttp = new XMLHttpRequest();
-			} else {
-				xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-			};
-			xmlhttp.open("POST","functions/submitfile.php");
-			xmlhttp.setRequestHeader("Content-Type",  "application/x-www-form-urlencoded");      
-			xmlhttp.send("img=" + dataurl + "filter=" + path);
+		ev.preventDefault();
+	}, false);
+
+	/**
+	 * AJAX REQ
+	 */
+
+	function send_cam_pic(dataurl, path) {
+		if (window.XMLHttpRequest) {
+			xmlhttp = new XMLHttpRequest();
+		} else {
+			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+		};
+		xmlhttp.open("POST", "functions/submitfile.php");
+		xmlhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xmlhttp.send("img=" + dataurl + "filter=" + path);
+	}
+
+	/**
+	 * IMAGE PNG EVENT
+	 */
+
+	fire.addEventListener('click', function (ev) {
+		if (path == "images/fire.png") {
+			return;
 		}
+		path = "images/fire.png";
+		fireborder.style.border = "1px solid black";
+		hatborder.style.border = "none";
+		beerborder.style.border = "none";
+		startbutton.disabled = false;
+		sendbutton.disabled = false;
+		img.onload = function () {
+			draw_png();
+		}
+		img.src = path;
+		ev.preventDefault();
+	}, false);
 
-		startbutton.addEventListener('click', function(ev){
-			takepicture();
-			ev.preventDefault();
-		}, false);
+	hat.addEventListener('click', function (ev) {
+		if (path == "images/hat.png") {
+			return;
+		}
+		path = "images/hat.png";
+		hatborder.style.border = "1px solid black";
+		fireborder.style.border = "none";
+		beerborder.style.border = "none";
+		startbutton.disabled = false;
+		sendbutton.disabled = false;
+		img.onload = function () {
+				draw_png();
+		}
+		img.src = path;
+		ev.preventDefault();
+	}, false);
 
-/**
- * IMAGE PNG EVENT
- */
-
-		fire.addEventListener('click', function(ev){
-			if (path == "images/fire.png") {
-				return ;
+	beer.addEventListener('click', function (ev) {
+		if (path == "images/beer.png") {
+			return;
+		}
+		path = "images/beer.png";
+		beerborder.style.border = "1px solid black";
+		fireborder.style.border = "none";
+		hatborder.style.border = "none";
+		startbutton.disabled = false;
+		sendbutton.disabled = false;
+			img.onload = function () {
+				draw_png();
 			}
-			path = "images/fire.png";
-			fireborder.style.border = "1px solid black";
-			hatborder.style.border = "none";
-			beerborder.style.border = "none";
-			startbutton.disabled = false;
-			sendbutton.disabled = false;
-			if (isplaying == true) {
-				img.onload = function() {
-					draw_png(path);
-				}
-				img.src = path;
-			}
-			ev.preventDefault();
-		}, false);
+			img.src = path;
+		ev.preventDefault();
+	}, false);
 
-		hat.addEventListener('click', function(ev){
-			if (path == "images/hat.png") {
-				return ;
-			}
-			path = "images/hat.png";
-			hatborder.style.border = "1px solid black";
-			fireborder.style.border = "none";
-			beerborder.style.border = "none";
-			startbutton.disabled = false;
-			sendbutton.disabled = false;
-			if (isplaying == true) {
-				img.onload = function() {
-					draw_png(path);
-				}
-				img.src = path;
-			}
-			ev.preventDefault();
-		}, false);
+	/**
+	 * MOVE PNG
+	 */
 
-		beer.addEventListener('click', function(ev){
-			if (path == "images/beer.png") {
-				return ;
-			}
-			path = "images/beer.png";
-			beerborder.style.border = "1px solid black";
-			fireborder.style.border = "none";
-			hatborder.style.border = "none";
-			startbutton.disabled = false;
-			sendbutton.disabled = false;
-			if (isplaying == true) {
-				img.onload = function() {
-					draw_png(path);
-				}
-				img.src = path;
-			}
-			ev.preventDefault();
-		}, false);
+	buttonD.addEventListener('click', function (ev) {
+		if (pngY < height) {
+			pngY += 10;
+		}
+		ev.preventDefault();
+	}, false);
 
-		/**
-		 * MOVE PNG
-		 */
+	buttonR.addEventListener('click', function (ev) {
+		if (pngX < width) {
+			pngX += 10;
+		}
+		ev.preventDefault();
+	}, false);
 
- 	
+	buttonL.addEventListener('click', function (ev) {
+		if (pngX > 0) {
+			pngX -= 10;
+		}
+		ev.preventDefault();
+	}, false);
 
-		buttonD.addEventListener('click', function(ev){
-			if (pngY < height) {
-				pngY += 10;
-			}
-			ev.preventDefault();
-		}, false);
+	buttonU.addEventListener('click', function (ev) {
+		if (pngY > 0) {
+			pngY -= 10;
+		}
+		ev.preventDefault();
+	}, false);
 
-		buttonR.addEventListener('click', function(ev){	
-			if (pngX < width) {
-				pngX += 10;
-			}
-			ev.preventDefault();
-		}, false);
+	/**
+	 * 
+	 * VIDEO DRAW 
+	 */
 
-		buttonL.addEventListener('click', function(ev){
-			if (pngX > 0) {
-				pngX -= 10;
-			}
-			ev.preventDefault();
-		}, false);
-
-		buttonU.addEventListener('click', function(ev){
-			if (pngY > 0) {
-				pngY -= 10;
-			}
-			ev.preventDefault();
-		}, false);
-		/**
-		 * 
-		 * VIDEO DRAW 
-		 */
-
-		function draw_video() {
-			(function loop() {
-				videoContext.drawImage(video, 0, 0, width, height);
+	function draw_video() {
+		(function loop() {
+			videoContext.drawImage(video, 0, 0, width, height);
 			setTimeout(loop, 1000 / 30); // drawing at 30fps
-			})();
-		}
-		function draw_png(path) {
-				(function loop() {
-					videoContext.drawImage(img, pngX, pngY);
-					setTimeout(loop, 1000 / 30); // drawing at 30fps
-				})();
-		}
+		})();
+	}
+	function draw_pic(pic) {
+		(function loop() {
+			videoContext.drawImage(pic, 0, 0, width, height);
+			setTimeout(loop, 1000 / 30); // drawing at 30fps
+		})();
+	}
+
+	function draw_png() {
+		(function loop() {
+			videoContext.drawImage(img, pngX, pngY);
+			setTimeout(loop, 1000 / 30); // drawing at 30fps
+		})();
 	}
 	window.addEventListener('load', startup, false);
-	})();
+})();
--- parts/picture.php
+++ parts/picture.php
@@ -1,29 +1,33 @@
 <div class="level">
-        <div class="column">
-            <table id="imgs">
-                <tr><th><div id="trFire"><a id="fire"><img src="images/fire.png"></img></a></div></th></tr>
-                <tr><th><div id="trHat"><a id="hat"><img src="images/hat.png"></img></a></div></th></tr>
-                <tr><th><div id="trBeer"><a id="beer"><img src="images/beer.png"></img></a></div></th></tr>
-            </table>
-        </div>
-        <div class="column">
-             <form method="post" action="functions/submitfile.php" enctype="multipart/form-data" >
-                <input type="file" name="myfile"/>
+        <div class="column" style="text-align: center;">
+            <div id="trFire"><a id="fire"><img src="images/fire.png"></img></a></div>
                 </br>
-                <input id="sendbutton" class="button is-link" type="submit" name="submit" value="Send" disabled/>
+            <div id="trHat"><a id="hat"><img src="images/hat.png"></img></a></div>
+                </br>
+            <div id="trBeer"><a id="beer"><img src="images/beer.png"></img></a></div>
+                </br>
+        </div>
+        <div class="column" style="border:1px solid black;text-align: center;">
+             <form enctype="multipart/form-data" >
+                <input type="file" name="myfile" id="filepng"/>
+                </br>
+            <button class="button is-link" id="sendbutton" disabled>Send</button>
             </form>
-            <canvas id="videoCanvas"></canvas>
 
+</br>
+            <canvas style="border:1px solid black;" id="videoCanvas"></canvas>
+</br>
             <input type="image" src="images/arrow_left.png" id="buttonLeft"></input>
             <input type="image" src="images/arrow_down.png" id="buttonDown"></input>
             <input type="image" src="images/arrow_up.png" id="buttonUp"></input>
             <input type="image" src="images/arrow_right.png" id="buttonRight"></input>
 
             <video style="width=400;height=400;" id="video" hidden></video>
+</br>
             <button class="button is-link" id="startbutton" disabled>Shoot</button>
         </div>
-        <div class="column">
-            <table id="tmp"></table>
+        <div class="column" style="text-align: center;">
+            <div id="table"></div>
         </div>
 </div>
 <script src="js/picture.js"></script>
